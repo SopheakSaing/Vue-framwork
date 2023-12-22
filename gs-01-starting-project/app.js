68095a29c20e6092252275190673cc9f4fa5db24
Pheak02 <saingsopheak02@gmail.com>
2023-12-22 17:20:05 +0700

    7. Re-building the App with Vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,13 +1,29 @@
-const buttonEl = document.querySelector("button");
-const inputEl = document.querySelector("input");
-const listEl = document.querySelector("ul");
+Vue.createApp({
+  data() {
+    //an obj data
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
 
-function addGoals() {
-  const enteredValue = inputEl.value; //the input has its value - vannila js
-  const listItemEl = document.createElement("li");  //to create el
-  listItemEl.textContent = enteredValue; //to enter value
-  listEl.appendChild(listItemEl); //append to listItemEl
-  inputEl.value = ""; //reset input to empty string again
-}
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
 
-buttonEl.addEventListener("click", addGoals);
+// function addGoals() {
+//   const enteredValue = inputEl.value; //the input has its value - vannila js
+//   const listItemEl = document.createElement("li");  //to create el
+//   listItemEl.textContent = enteredValue; //to enter value
+//   listEl.appendChild(listItemEl); //append to listItemEl
+//   inputEl.value = ""; //reset input to empty string again
+// }
+
+// buttonEl.addEventListener("click", addGoals);
