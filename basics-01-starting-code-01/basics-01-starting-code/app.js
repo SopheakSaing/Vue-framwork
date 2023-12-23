763a1c65d18d42dc1d6a95ee6ecbf20603f68a3c
Pheak02 <saingsopheak02@gmail.com>
2023-12-23 10:10:37 +0700

    17. Understanding "methods" in Vue Apps

--- basics-01-starting-code-01/basics-01-starting-code/app.js
+++ basics-01-starting-code-01/basics-01-starting-code/app.js
@@ -2,8 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       courseGoal: "Finish the course and can apply to real project!",
-      vueLink: 'https://vuejs.org/'
+      vueLink: "https://vuejs.org/",
     };
+  },
+  methods: {
+    outputGoal() {
+      const radomNum = Math.random();
+      if (radomNum < 0.5) {
+        return "Learn Vue";
+      } else {
+        return "master vue!";
+      }
+    },
   },
 });
 
