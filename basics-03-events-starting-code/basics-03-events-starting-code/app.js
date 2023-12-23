68964cc92fa1928ba8a6a1f11295acb28235a821
Pheak02 <saingsopheak02@gmail.com>
2023-12-23 15:00:38 +0700

    24. Using the Native Event Object

--- basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -2,9 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods: {
+    // setName(event) {
+    //   this.name = event.target.value;
+    // },
+    setName(event, surname) {
+      //get event from browser
+      this.name = event.target.value + " " + surname;
+    },
     increment(num) {
       return (this.counter = this.counter + num);
     },
