2ffe4999c10b67513032d415708da8498ae028e6
Pheak02 <saingsopheak02@gmail.com>
2023-12-28 22:05:29 +0700

    70. Understanding the Problem

--- 06_cmp-intro-01-starting-setup/cmp-intro-01-starting-setup/app.js
+++ 06_cmp-intro-01-starting-setup/cmp-intro-01-starting-setup/app.js
@@ -1,10 +1,27 @@
 const app = Vue.createApp({
   data() {
     return {
-      friends: {
-        id: "pheak",
-      },
+      detailsAreVisible: false,
+      friends: [
+        {
+          id: "1",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "2",
+          name: "nobody",
+          phone: "01234 4678 5533",
+          email: "manuel01@gmail.com",
+        },
+      ],
     };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
   },
 });
 app.mount("#app");
