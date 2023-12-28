9d4475e71c7cafc0bcfa08d222f492e4a7092437
Pheak02 <saingsopheak02@gmail.com>
2023-12-28 22:35:34 +0700

    1. Introducing Components

--- 06_cmp-intro-01-starting-setup/cmp-intro-01-starting-setup/app.js
+++ 06_cmp-intro-01-starting-setup/cmp-intro-01-starting-setup/app.js
@@ -4,18 +4,38 @@ const app = Vue.createApp({
       detailsAreVisible: false,
       friends: [
         {
-          id: "1",
-          name: "Manuel Lorenz",
-          phone: "01234 5678 991",
-          email: "manuel@localhost.com",
-        },
-        {
           id: "2",
           name: "nobody",
           phone: "01234 4678 5533",
           email: "manuel01@gmail.com",
         },
       ],
+    };
+  },
+});
+
+app.component("friend-contact", {
+  template: ` 
+  <li>
+    <h2>{{friend.name}}</h2>
+    <button @click="toggleDetails">
+      {{detailsAreVisible? 'Hide' : 'Show'}} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{friend.phone}}</li>
+      <li><strong>Email:</strong> {{friend.email}}</li>
+    </ul>
+  </li>
+`,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "1",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
     };
   },
   methods: {
