7ef94befd7f1d0f6268ba8544eddf8795e8164ba
Pheak02 <saingsopheak02@gmail.com>
2023-12-29 09:39:15 +0700

    85. Creating a Basic Vue App

---
+++ 07_vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<!-- regular html markup -->
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            friends:[
+                {
+                    id: '1',
+                    name:"mony",
+                    phone:'0110 10101',
+                    email:"abc@gmail.com"
+
+                },
+                {
+                    id: '1',
+                    name:"sophie",
+                    phone:'0110 100101',
+                    email:"sophie@gmail.com"
+
+                }
+            ]
+        }
+    }
+}
+</script>
