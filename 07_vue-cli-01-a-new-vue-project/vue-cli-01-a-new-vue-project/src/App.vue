e2e487fafa870270fbc93817e4e5419eab35169b
Pheak02 <saingsopheak02@gmail.com>
2023-12-29 10:11:16 +0700

    86. Adding a Component

--- 07_vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/App.vue
+++ 07_vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,33 +1,31 @@
 <!-- regular html markup -->
 <template>
-    <section>
-        <h2>My Friends</h2>
-        <ul>
-            <li></li>
-        </ul>
-    </section>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            friends:[
-                {
-                    id: '1',
-                    name:"mony",
-                    phone:'0110 10101',
-                    email:"abc@gmail.com"
-
-                },
-                {
-                    id: '1',
-                    name:"sophie",
-                    phone:'0110 100101',
-                    email:"sophie@gmail.com"
-
-                }
-            ]
-        }
-    }
-}
+  data() {
+    return {
+      friends: [
+        {
+          id: "1",
+          name: "mony",
+          phone: "0110 10101",
+          email: "abc@gmail.com",
+        },
+        {
+          id: "1",
+          name: "sophie",
+          phone: "0110 100101",
+          email: "sophie@gmail.com",
+        },
+      ],
+    };
+  },
+};
 </script>
---
+++ 07_vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "1",
+        name: "mony",
+        phone: "0110 10101",
+        email: "abc@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
