46fcee3e7983a250c3dd260b1d993c4dccfa77de
Pheak02 <saingsopheak02@gmail.com>
2024-02-22 01:27:24 +0700

    Stop 111. Global vs Local Components

--- 09_cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/App.vue
+++ 09_cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/App.vue
@@ -11,15 +11,24 @@
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+
 export default {
+  components: {
+    "the-header": TheHeader,
+    "badge-list": BadgeList,
+    "user-info": UserInfo,
+  },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
-    }; 
+    };
   },
 };
 </script>
