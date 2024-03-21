15a115432d0ec63f1a4de793fecf03b43344df37
Pheak02 <saingsopheak02@gmail.com>
2024-03-22 02:08:08 +0700

    120. Applying What We Know & A Problem

--- 09_cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ 09_cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,5 +1,36 @@
 <!-- user can add new course goals -->
 <template>
   <h2>Manage Goals</h2>
-  <input type="text" />
+  <input type="text" ref="goal" />
+  <button @click="setGoals">Set Goals</button>
+  <error-alert-modal v-if="inputIsInvalid">
+    <h2>Input is Invalid</h2>
+    <p>Plz enter more characters!</p>
+    <button @click="confirmError()">Okeh</button>
+  </error-alert-modal>
 </template>
+<script>
+import ErrorAlertModal from "./ErrorAlertModal.vue";
+
+export default {
+  components: {
+    ErrorAlertModal,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoals() {
+      const enteredvalue = this.$refs.goal.value;
+      if (enteredvalue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
