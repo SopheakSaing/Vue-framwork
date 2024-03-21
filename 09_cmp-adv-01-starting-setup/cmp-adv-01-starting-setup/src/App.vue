7db157d0074bcdaa615335f4f145764ed754c836
Pheak02 <saingsopheak02@gmail.com>
2024-03-22 01:27:39 +0700

    118. Dynamic Components

--- 09_cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/App.vue
+++ 09_cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/App.vue
@@ -1,40 +1,42 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{ slotProps["another-prop"] }}</p>
-    </course-goals>
+    <!-- <TheHeader/> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>
+    <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
 
 export default {
   components: {
-    "the-header": TheHeader,
-    "badge-list": BadgeList,
-    "user-info": UserInfo,
-    "course-goals": CourseGoals,
+    TheHeader,
+    ManageGoals,
+    ActiveGoals,
   },
   data() {
     return {
+      selectedComponent: "active-goals", //active-goals as HTML tag name
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
---
+++ 09_cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -0,0 +1,4 @@
+<!-- user can add new course goals -->
+<template>
+  <h2>Manage Goals</h2>
+</template>
