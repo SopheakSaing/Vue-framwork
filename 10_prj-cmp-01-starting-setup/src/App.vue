8f16260fe2436ea22bcac46ff1a970d43a30a996
Sopheak Saing <sopheak.saing@khalibre.com>
2024-03-27 15:42:48 +0700

    129. Setup & First Steps

---
+++ 10_prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,21 @@
+<template><ul></ul></template>
+<script>
+export default{
+    data() {
+        return {
+            stroedRes: [{
+                id: 'official',
+                title: 'office guid',
+                description: "loremabc",
+                link: "https://vuejs.org"
+            },
+            {
+                id: 'google',
+                title: 'google',
+                description: "loremabcde",
+                link: "https://google.com"
+            }]
+        }
+    }
+}
+</script>
