fb098856c305390204b6f21437b117facc840875
Pheak02 <saingsopheak02@gmail.com>
2024-04-06 22:34:50 +0700

    restart s10 & 130. First Components & Props

--- 10_prj-cmp-01-starting-setup/src/App.vue
+++ 10_prj-cmp-01-starting-setup/src/App.vue
@@ -1,46 +1,37 @@
 <template>
-  <the-header title="RememberMe"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.key"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
 </template>
 <script>
-// import LearningResource from './components/learning-resources/LearningResource.vue';
-import TheHeader from './components/layouts/TheHeader.vue';
-
+import LearningResource from './components/learning-resource/LearningResource.vue';
 export default {
-  components: [TheHeader],
+  components: {
+    LearningResource,
+  },
   data() {
     return {
       storedResources: [
         {
-          id: 'official',
-          title: 'office guid',
-          description: 'loremabc',
-          link: 'https://vuejs.org',
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js docs',
+          link: 'https://vuejs/org',
         },
         {
-          id: 'google',
-          title: 'google',
-          description: 'loremabcde',
-          link: 'https://google.com',
+          id: 'nearly official-guide',
+          title: 'ABC Guide',
+          description: 'The official react.js docs',
+          link: 'https://reactjs/org',
         },
       ],
     };
   },
 };
 </script>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-
-* {
-  box-sizing: border-box;
-}
-
-html {
-  font-family: 'Roboto', sans-serif;
-}
-
-body {
-  margin: 0;
-}
-</style>
