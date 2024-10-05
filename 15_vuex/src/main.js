3f1cfd38525d86238929245222fa953dd2f6fb56
Pheak02 <saingsopheak02@gmail.com>
2024-10-05 16:21:51 +0700

    219. Introducing Getters - A Better Way Of Getting Data

--- 15_vuex/src/main.js
+++ 15_vuex/src/main.js
@@ -1,24 +1,40 @@
 import { createApp } from 'vue';
 import { createStore } from 'vuex';
 
-
 import App from './App.vue';
 
 const store = createStore({
-    state(){
-        return {
-            counter: 0
-        };
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+      //this state param will garantee by vuex
+      state.counter = state.counter + 2;
     },
-    mutations: {
-      increment(state) { //this state param will garantee by vuex
-        state.counter = state.counter + 2;
-      },
-      increase(state, payload){
-        state.counter = state.counter + payload.value;
-      }  
-    }
-})
-const app = createApp(App)
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+      console.log(payload)
+    },
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 3;
+    },
+    normalizedCounter(_, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) {
+        return 0;
+      }
+      if (finalCounter > 100) {
+        return 100;
+      }
+      return finalCounter;
+    },
+  },
+});
+const app = createApp(App);
 app.use(store);
 app.mount('#app');
