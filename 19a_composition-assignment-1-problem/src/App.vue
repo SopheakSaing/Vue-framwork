11cc2960edc64cb590f15b675445d0cc690c6f78
Pheak02 <saingsopheak02@gmail.com>
2024-10-15 20:20:25 +0700

    solve toggle course goal with separate ref

--- 19a_composition-assignment-1-problem/src/App.vue
+++ 19a_composition-assignment-1-problem/src/App.vue
@@ -2,9 +2,9 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-if="isShow">{{ courseGoal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="showCourse">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,9 +13,19 @@
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
-  
-}
+  setup() {
+    const courseGoal = ref('Apply composition API with Vue3 and liferay');
+    const isShow = ref(false);
+
+    const showCourse = () => {
+      isShow.value = !isShow.value;
+    };
+
+    return { courseGoal, isShow,  showCourse };
+  },
+};
 </script>
 
 <style>
