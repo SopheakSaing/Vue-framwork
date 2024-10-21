e4858c7a40958d7470818b493f06328f1be14fed
Pheak02 <saingsopheak02@gmail.com>
2024-10-21 22:08:29 +0700

    295. Working with Watchers

--- 19_composition-01-starting-setup/src/App.vue
+++ 19_composition-01-starting-setup/src/App.vue
@@ -1,71 +1,77 @@
 <template>
-    <section class="container">
-        <h2>{{ fullName }}</h2>
-        <h2>{{ age }}</h2>
-        <button @click="setNewAge()">Change Age</button>
-        <div>
-            <input type="text" placeholder="First Name" @input="setFirstName">
-            <input type="text" placeholder="Last Name" @input="setLastName">
-        </div>
-    </section>
+  <section class="container">
+    <h2>{{ fullName }}</h2>
+    <h2>{{ age }}</h2>
+    <button @click="setNewAge()">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" v-model="lastName" />
+    </div>
+  </section>
 </template>
 
 <script>
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue';
 export default {
-    setup() {
-        // const userName = ref('Meisme Alpha test')
-        // const userAge = ref(20)
-        const firstName = ref('');
-        const lastName = ref('')
-        const age = ref(31);
+  setup() {
+    // const userName = ref('Meisme Alpha test')
+    // const userAge = ref(20)
+    const firstName = ref('');
+    const lastName = ref('');
+    const age = ref(31);
 
-        
-        function setNewAge() {
-            age.value = ref('32')
-        }
+    const fullName = computed(function () {
+      return firstName.value + ' ' + lastName.value;
+    });
 
-        computed(function() {
-            return firstName.value + ' ' + lastName.value
-        });
 
-        function setFirstName(event) {
-            firstName.value = event.target.value;
-        }
-
-        
-        const fullName = computed(function()  {
-            return firstName.value + ' ' + lastName.value
-        });
-
-        function setLastName(event) {
-            lastName.value = event.target.value;
-        }
-
-        return { age, fullName, setFirstName, setLastName, setNewAge} //not executing the func() but storing its pointer
+    function setNewAge() {
+      age.value = 32;
     }
+
+    watch( [age, fullName], function (newValues, oldValues) {
+      //whenever age changes, that the watch func execute
+      console.log('Old age:' + oldValues[0]);
+      console.log('New age:' + newValues[0]);
+           console.log('Old name:' + oldValues[1]);
+      console.log('New name:' + newValues[1]);
+    });
+
+    computed(function () {
+      return firstName.value + ' ' + lastName.value;
+    });
+
+    // function setFirstName(event) {
+    //     firstName.value = event.target.value;
+    // }
+
+    // function setLastName(event) {
+    //     lastName.value = event.target.value;
+    // }
+    return { age, fullName, setNewAge, firstName, lastName }; //not executing the func() but storing its pointer
+  },
 };
 </script>
 
 <style>
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 html {
-    font-family: sans-serif;
+  font-family: sans-serif;
 }
 
 body {
-    margin: 0;
+  margin: 0;
 }
 
 .container {
-    margin: 3rem auto;
-    max-width: 30rem;
-    border-radius: 12px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-    padding: 1rem;
-    text-align: center;
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
 }
 </style>
