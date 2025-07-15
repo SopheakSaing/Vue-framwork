0b77d54236adefe94917e91ae1f87ff65dbbc493
Pheak02 <saingsopheak02@gmail.com>
2025-07-15 22:32:37 +0700

    ex tracker: cleanup file, add staic UI, init with option API

---
+++ PROJECT/src/components/TransactionList.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li v-for="transaction in transactions"
+    :key="transaction.id"
+    :class="transaction.amount < 0 ? 'minus' : 'plus'">
+      {{transaction.item}} <span>{{transaction.amount}}</span>
+      <button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        {id: 1, item: 'Milk', amount: -5},
+        {id: 2, item: 'Food', amount: -20},
+        {id: 3, item: 'Fruit', amount: -30},
+        {id: 4, item: 'skirt', amount: -100},
+        {id: 4, item: 'basic salary', amount: 10000},
+      ]
+    }
+  }
+}
+</script>
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Vue-framwork
 Learn from Udemy Course!
+
+COMMIT MESSAGE PREFIX:
+* learn: conceptual understanding
+* practice: put concept into a practical project
