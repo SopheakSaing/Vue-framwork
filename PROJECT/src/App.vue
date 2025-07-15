ed8cabf10f10b8a2e984149af7493dd1c9eae912
Pheak02 <saingsopheak02@gmail.com>
2025-07-15 22:53:49 +0700

    ex tracker: move to global state

--- PROJECT/src/App.vue
+++ PROJECT/src/App.vue
@@ -1,14 +1,17 @@
-<script>
-import {defineComponent} from "vue";
+<script setup>
 import Header from "@/components/Header.vue";
 import Balance from "@/components/Balance.vue";
 import IncomeExpense from "@/components/IncomeExpense.vue";
 import TransactionList from "@/components/TransactionList.vue"
 import TransactionForm from "@/components/TransactionForm.vue";
-
-export default defineComponent({
-  components: {TransactionForm, TransactionList, IncomeExpense, Balance, Header}
-})
+import {ref} from "vue";
+const transactions = ref([ //want this to reactive
+  {id: 1, item: 'Milk', amount: -5},
+  {id: 2, item: 'Food', amount: -20},
+  {id: 3, item: 'Fruit', amount: -30},
+  {id: 4, item: 'skirt', amount: -100},
+  {id: 4, item: 'basic salary', amount: 10000},
+])
 
 </script>
 <template>
@@ -16,7 +19,7 @@ export default defineComponent({
   <div class="container">
     <Balance/>
     <IncomeExpense/>
-    <TransactionList/>
+    <TransactionList :transactions="transactions"/>
     <TransactionForm/>
   </div>
 </template>
--- PROJECT/src/components/TransactionList.vue
+++ PROJECT/src/components/TransactionList.vue
@@ -10,18 +10,12 @@
     </li>
   </ul>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      transactions: [
-        {id: 1, item: 'Milk', amount: -5},
-        {id: 2, item: 'Food', amount: -20},
-        {id: 3, item: 'Fruit', amount: -30},
-        {id: 4, item: 'skirt', amount: -100},
-        {id: 4, item: 'basic salary', amount: 10000},
-      ]
-    }
+<script setup>
+import {defineProps} from 'vue';
+const props = defineProps({
+  transactions: {
+    type:   Array,
+    required: true,
   }
-}
+})
 </script>
