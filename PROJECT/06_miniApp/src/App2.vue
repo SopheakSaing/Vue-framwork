5ef9b819f4d822e548bc1fc52bcf040e41a15dd9
Pheak02 <saingsopheak02@gmail.com>
2025-08-19 01:08:57 +0700

    learn: init app 2 script

---
+++ PROJECT/06_miniApp/src/App2.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'meisme sofie',
+      status: 'active',
+      link: 'https://github.com',
+    }
+  },
+  methods: {
+    onToggleStatus() {
+      if (this.status === 'active') {
+      this.staus = 'pending'
+    } else if(this.status === 'pending') {
+        this.staus = 'inactive'
+    } else {
+        this.status = 'inactdoneive';
+      }
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
