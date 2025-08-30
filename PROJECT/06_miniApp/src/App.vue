8442d917e8bf4bc27782d74a33584534c6450b41
Pheak02 <saingsopheak02@gmail.com>
2025-08-30 23:38:06 +0700

    learn: add homecard

--- PROJECT/06_miniApp/src/App.vue
+++ PROJECT/06_miniApp/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import NavBar from "@/components/NavBar.vue";
 import Hero from "@/components/Hero.vue";
+import HomeCard from "@/components/HomeCards.vue";
 </script>
 <template>
   <NavBar/>
   <Hero/>
+  <HomeCard/>
 </template>
