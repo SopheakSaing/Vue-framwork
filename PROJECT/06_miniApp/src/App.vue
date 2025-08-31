5555a22cca755f70cf2ac641b902a154c7679d7d
Pheak02 <saingsopheak02@gmail.com>
2025-08-31 16:13:34 +0700

    learn: just job listing limit and show button prop

--- PROJECT/06_miniApp/src/App.vue
+++ PROJECT/06_miniApp/src/App.vue
@@ -2,9 +2,11 @@
 import NavBar from "@/components/NavBar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCard from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
 </script>
 <template>
   <NavBar/>
   <Hero/>
   <HomeCard/>
+  <JobListings :limit="3" :show-button="true"/>
 </template>
