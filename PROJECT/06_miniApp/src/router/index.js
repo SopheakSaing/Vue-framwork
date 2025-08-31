cc4a4623b7f2bcbfd65a712c080fe2c9935e620d
Pheak02 <saingsopheak02@gmail.com>
2025-08-31 22:50:27 +0700

    learn: not found page

--- PROJECT/06_miniApp/src/router/index.js
+++ PROJECT/06_miniApp/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import JobViews from "@/views/JobViews.vue";
+import JobsView from "@/views/JobsView.vue";
+import NotFoundView from '@/views/NotFoundView.vue'
+import JobView from "@/views/JobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +15,17 @@ const router = createRouter({
     {
       path: '/jobs',
       name: 'jobs',
-      component: JobViews
+      component: JobsView
+    },
+    {
+      path: '/jobs/:id',
+      name: 'job',
+      component: JobView
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
     },
   ]
 })
